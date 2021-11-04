e92ee044ea5003cafacfea5ef7d683a5e813c9de
Adrienne Detwiler <adrienne@mobiusconsortium.org>
2021-11-04 11:07:25 -0500

    Updated 856links.js - RT 154763

--- 856links.js
+++ 856links.js
@@ -2,12 +2,12 @@ function hideLinks(){
     
     var stringsToHide = new Array ();
     stringsToHide[1] = new Array ("centralmethodist");
-    stringsToHide[3] = new Array ("Missouri Valley eBook", "moval");
-	stringsToHide[4] = new Array ("State Fair eBook");
-	stringsToHide[6] = new Array ("atsu");
+    stringsToHide[3] = new Array ("Missouri Valley", "moval");
+	stringsToHide[4] = new Array ("State Fair", "SFCC");
+	stringsToHide[6] = new Array ("A.T. Still", "atsu");
 	stringsToHide[7] = new Array ("Culver-Stockton");
 	stringsToHide[8] = new Array ("hlg");
-	stringsToHide[9] = new Array ("State Tech");
+	stringsToHide[9] = new Array ("State Tech", "STATE TECH");
 	stringsToHide[10] = new Array ("MACC");
 	stringsToHide[11] = new Array ("Truman", "truman");
     
